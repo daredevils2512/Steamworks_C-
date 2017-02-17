91e29a5fd3b7be85c9d993bd0f9a1f9c359be378
gamewizard75 <troymartin100@gmail.com>
2017-02-17 10:36:38 -0600

    Worked on the shootervisiontrack command.

--- src/Commands/ShooterVisionTrack.cpp
+++ src/Commands/ShooterVisionTrack.cpp
@@ -9,7 +9,7 @@ ShooterVisionTrack::ShooterVisionTrack() {
 	// Use Requires() here to declare subsystem dependencies
 	// eg. Requires(Robot::chassis.get());
 	abort = false;
-	speed = 0.5;
+	speed = 0.8;
 	fwdLastPressed = false;
 	revLastPressed = false;
 	fwdLastPassed = false;
@@ -31,12 +31,8 @@ void ShooterVisionTrack::Execute() {
 	std::vector<PixySubsystem::ObjectValues> frame = Robot::pixySubsystem->GetShooterPixyData();
 	std::cout << frame.size() << std::endl;
 	if(frame.size() == 0){
-		if (FwdPressedThisTime() || FwdEncPassedThisTime() ||
-				RevPressedThisTime() || RevEncPassedThisTime()) {
-//			std::cout << "reverse pressed this time: " << RevPressedThisTime() << std::endl;
-//			std::cout << "forward pressed this time: " << FwdPressedThisTime() << std::endl;
-//			std::cout << "forward encoder passed this time: " << FwdEncPassedThisTime() << std::endl;
-//			std::cout << "reverse encoder passed this time: " << RevEncPassedThisTime() << std::endl;
+		if (/*FwdPressedThisTime() ||*/ FwdEncPassedThisTime() ||
+				/*RevPressedThisTime() ||*/ RevEncPassedThisTime()) {
 			std::cout << "switch speed" << std::endl;
 			speed = -speed; //reverse the speed
 
@@ -142,11 +138,11 @@ void ShooterVisionTrack::Execute() {
 		std::cout << "tolerance acceptable" << std::endl;
 	}else if(trackedObj.x < middle){
 		// left of middle
-		std::cout << "+" << std::endl;
+		std::cout << "-" << std::endl;
 		Robot::shooter->SetSwivelSpeed(-0.6);
 	}else{
 		// hopefully to the right of middle
-		std::cout << "-" << std::endl;
+		std::cout << "+" << std::endl;
 		Robot::shooter->SetSwivelSpeed(0.6);
 	}
 
